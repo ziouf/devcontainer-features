0e267ba97f5eca600518921b8313cdbeb724af4f
Cyril MARIN <marin.cyril@gmail.com>
2025-05-08 08:28:07 +0000

    feat(tailwindcss-standalone): add tailwindcss-standalone

---
+++ src/tailwindcss-standalone/install.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+set -e
+
+echo "Installing natscli..."
+
+VERSION=${VERSION:-latest}
+echo "The version to install is: $VERSION"
+
+
+REPOSITORY=tailwindlabs/tailwindcss
+
+if [ "$VERSION" = "latest" ]; then
+  VERSION=$(curl -s "https://api.github.com/repos/${REPOSITORY}/releases/latest" | grep tag_name | cut -d '"' -f 4)
+  echo "Latest version is: $VERSION"
+fi
+
+# Trim the leading "v" from the version string
+VERSION=${VERSION#v}
+
+# Determine the OS and architecture
+case "$(uname -s | tr '[:upper:]' '[:lower:]')" in
+  linux*)
+    OS="linux"
+    ;;
+  darwin*)
+    OS="macos"
+    ;;
+  *)
+    echo "Unsupported OS: $OS"
+    exit 1
+    ;;
+esac
+
+case "$(uname -m)" in
+  x86_64)
+    ARCH="x64"
+    ;;
+  aarch64)
+    ARCH="arm64"
+    ;;
+  *)
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+    ;;
+esac
+
+
+# Construct the download URL
+URL="https://github.com/${REPOSITORY}/releases/download/v${VERSION}/tailwindcss-${OS}-${ARCH}"
+
+# Download the binary
+echo "Downloading natscli from $URL"
+curl -L -o /usr/local/bin/tailwindcss "$URL"
+chmod +x /usr/local/bin/tailwindcss
